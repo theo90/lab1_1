0b974b1afc2e29550c323dbf849c674a70b26868
Goetz Lehmann <goetz@tp1.uni-duesseldorf.de>
2015-10-21 15:52:57 +0200

    preparation3

---
+++ hello.cxx
@@ -0,0 +1,23 @@
+/*
+ * hello.cxx
+ *
+ *  Created on: 09.04.2013
+ *      Author: goetz
+ */
+
+// single line comment
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "Hello World" << endl;
+
+	return 0;
+}
+
+
+
